6a8b523b697121750cf5250d3f0e181e5fa5486e
Tunde James <tundejames04@gmail.com>
2023-11-08 15:34:18 +0100

    Create the following components: header, hero-section

---
+++ app/home/hero-section.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function HeroSection() {
+  return (
+    <section className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="col-span-7 w-full place-self-center justify-self-start text-center sm:text-left">
+        <h1 className="text-2xl capitalize sm:text-4xl md:text-6xl md:leading-normal">
+          tech news for busy peeps
+        </h1>
+        <button className="mt-3 block w-fit rounded border-2 bg-indigo-500 px-4 py-2 text-white shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)] sm:px-6 sm:py-4">
+          browse articles
+        </button>
+      </div>
+
+      <div className="col-span-5 place-self-center">
+        <Image
+          src="/coffee-call.png"
+          width={300}
+          height={300}
+          alt="person walking with a coffee cup while on a call"
+        />
+      </div>
+    </section>
+  );
+}
---
+++ app/ui/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="border-b border-black flex justify-between">
+      <div className="text-4xl capitalize px-4 py-2">neo</div>
+      <button className="bg-black text-white capitalize px-2 py-4">
+        sign in
+      </button>
+    </header>
+  );
+}
