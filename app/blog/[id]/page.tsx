9631797d68e07d8a90908df4172052156d946445
Tunde James <tundejames04@gmail.com>
2023-11-09 13:23:22 +0100

    Update the blog post form and install UploadThings

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,18 +1,30 @@
 import { prisma } from "@/lib/prisma";
+import Image from "next/image";
 
 async function page({ params }: { params: { id: string } }) {
   const post = await prisma.post.findUnique({
     where: {
       id: Number(params.id),
     },
+    include: {
+      author: true,
+    },
   });
 
   return (
-    <div>
+    <div className="container-sm mt-6">
       {post && (
         <>
-          <h1>{post.title}</h1>
-          <p className="">{post.content}</p>
+          <h1 className="mb-5 text-5xl">{post.title}</h1>
+          <div className="flex items-center">
+            <p className="text-sm text-gray-500">By</p>
+            <div
+              className="ml-3 mr-2 h-5 w-5 rounded-full bg-contain"
+              style={{ backgroundImage: `url(${post.author.image})` }}
+            ></div>
+            <p className="text-sm text-gray-500">{post.author.name}</p>
+          </div>
+          <p className="mt-4 text-sm text-gray-500">{post.content}</p>
         </>
       )}
     </div>
