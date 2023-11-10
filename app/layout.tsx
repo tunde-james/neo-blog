04c9b72b81f29ae78e92b63727141e57f850947b
Tunde James <tundejames04@gmail.com>
2023-11-10 10:54:51 +0100

    Fix the height issue for individual post

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>{children}</main>
+        <main className="min-h-full">{children}</main>
         <Footer />
       </body>
     </html>
